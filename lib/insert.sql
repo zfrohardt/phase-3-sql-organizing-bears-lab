8cd39ed44ca09dcd04540239f94a86cd2a773797
Zach Frohardt <zfrohardt@gmail.com>
2021-05-24 11:14:34 -0700

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ("Mr. Chocolate", 20, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Rowdy", 10, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Tabitha", 4, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Sergeant Brown", 2, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Melissa", 13, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Grinch", 2, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Wendy", 6, "F");
+INSERT INTO bears (age, sex) VALUES (2, "M");
